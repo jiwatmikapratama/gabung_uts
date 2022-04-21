009fae043734ac43d7b35d605c7217347a7ead62
jiwatmikapratama <jiwatmikapratama@gmail.com>
2022-04-21 16:31:51 +0800

    Menambahkan map dan search

--- lib/pages/beranda_login.dart
+++ lib/pages/beranda_login.dart
@@ -1,95 +1,106 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:gabung_uts/main.dart';
 import 'package:gabung_uts/pages/daftar_pengelola.dart';
 import 'package:gabung_uts/pages/pengaturan.dart';
 import 'package:gabung_uts/pages/riwayat.dart';
 import 'package:gabung_uts/pages/detail_profil.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:gabung_uts/src/locations.dart' as locations;
 
-class Beranda_login extends StatelessWidget {
+class Beranda_login extends StatefulWidget {
   const Beranda_login({Key? key}) : super(key: key);
+
+  @override
+  State<Beranda_login> createState() => _Beranda_loginState();
+}
+
+class _Beranda_loginState extends State<Beranda_login> {
+  final Map<String, Marker> _markers = {};
+  Future<void> _onMapCreated(GoogleMapController controller) async {
+    final googleOffices = await locations.getGoogleOffices();
+    setState(() {
+      _markers.clear();
+      for (final office in googleOffices.offices) {
+        final marker = Marker(
+          markerId: MarkerId(office.name),
+          position: LatLng(office.lat, office.lng),
+          infoWindow: InfoWindow(
+            title: office.name,
+            snippet: office.address,
+          ),
+        );
+        _markers[office.name] = marker;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          iconTheme: IconThemeData(color: Colors.black),
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          title: Container(
-            alignment: Alignment.center,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(10),
-                ),
-                color: Color(0xFF00FF85),
+        backgroundColor: Color(0xFF189AB4),
+        title: Text('Search'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              showSearch(context: context, delegate: DataSearch());
+            },
+          )
+        ],
+      ),
+      // body: Container(
+      body: Center(
+        child: Stack(
+          children: [
+            GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: LatLng(0, 0),
+                zoom: 11.0,
               ),
-              width: double.infinity,
-              padding: EdgeInsets.all(5),
+              markers: _markers.values.toSet(),
+            ),
+            Container(
+              alignment: Alignment.topCenter,
               child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                  color: Color(0xFF00FF85),
+                ),
                 width: double.infinity,
-                height: 30,
-                child: GestureDetector(
-                  onTap: () async {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => Daftar_pengelola()),
-                    );
-                  },
-                  child: Center(
+                padding: EdgeInsets.all(5),
+                child: Container(
+                  width: double.infinity,
+                  height: 30,
+                  child: GestureDetector(
+                    onTap: () async {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => Daftar_pengelola()),
+                      );
+                    },
+                    child: Center(
                       child: Text(
-                    "Daftar Sebagai Pengelola?",
-                    style: TextStyle(color: Colors.black, fontSize: 18),
-                  )),
+                        "Daftar Sebagai Pengelola?",
+                        style: TextStyle(color: Colors.black, fontSize: 18),
+                      ),
+                    ),
+                  ),
                 ),
               ),
             ),
-          )),
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        decoration: BoxDecoration(
-            image: DecorationImage(
-          image: AssetImage(
-            "images/fix.png",
-          ),
-          fit: BoxFit.fill,
-        )),
-        child: Container(
-          height: 100,
-          margin: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 660),
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.black,
-              width: 2,
-            ),
-            borderRadius: BorderRadius.all(Radius.circular(50)),
-            color: Color(0xFF189AB4),
-          ),
-          alignment: Alignment.topCenter,
-          child: Row(
-            children: [
-              Container(
-                alignment: Alignment.center,
-                padding: EdgeInsets.only(left: 10),
-                child: Icon(Icons.search, color: Colors.white),
-              ),
-              Container(
-                alignment: Alignment.center,
-                padding: EdgeInsets.only(left: 10),
-                child: Text("Search",
-                    style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.white)),
-              )
-            ],
-          ),
+          ],
         ),
       ),
+
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
@@ -137,8 +148,8 @@ class Beranda_login extends StatelessWidget {
                   color: Colors.black,
                 ),
               ),
-              onTap: () => Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => Pengaturan())),
+              onTap: () => Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => Pengaturan())),
             ),
             ListTile(
               leading: Icon(
@@ -157,6 +168,72 @@ class Beranda_login extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class DataSearch extends SearchDelegate<String> {
+  final pura = ["Besakih", "Tanah Lot", "Uluwatu"];
+
+  final recentPura = ["Besakih", "Tanah Lot"];
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    // TODO: implement buildActions
+    return [
+      IconButton(
+        onPressed: () {
+          query = "";
+        },
+        icon: Icon(Icons.clear),
+      )
+    ];
+
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    // TODO: implement buildLeading
+    return IconButton(
+      onPressed: () {
+        close(context, "");
+      },
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
+      ),
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // TODO: implement buildResults
+
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // TODO: implement buildSuggestions
+    final suggestionList = query.isEmpty
+        ? recentPura
+        : pura.where((p) => p.startsWith(query)).toList();
+
+    return ListView.builder(
+      itemBuilder: (context, index) => ListTile(
+          onTap: () {
+            showResults(context);
+          },
+          leading: Icon(Icons.location_city),
+          title: RichText(
+              text: TextSpan(
+            text: suggestionList[index].substring(0, query.length),
+            style: TextStyle(
+              color: Colors.grey,
+            ),
+          ))),
+      itemCount: suggestionList.length,
     );
   }
 }
